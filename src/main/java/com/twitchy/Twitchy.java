31a55ee4e41c00333719b915afc290c878aed08f
Dragonhatch1 <Dragonhatch1@hotmail.com>
2026-08-11 03:44:13 -0500

    TODOs

--- src/main/java/com/twitchy/Twitchy.java
+++ src/main/java/com/twitchy/Twitchy.java
@@ -58,6 +58,6 @@ public class Twitchy {
 // TODO Death Captcha for Bits
 // TODO Mob Spawning for Bits
 // TODO Spawn fish in Fishing grounds for 1-2k channel points.
-// TODO Spawn entity on Sub | Fish in ball that rolls around to follow you | can roam when your not near it
+// TODO Spawn entity on Sub | Twilight forest Entity | Ravens, Tiny Birds,
 // TODO Add failure of Channel Point Captcha
 // TODO Pong as a captcha?
